894ed619d9d8e44d517132e36008e281fb5744d6
Rodrigo <digo_santiago@hotmail.com>
2020-05-31 14:15:25 -0300

    kernel's first compilation

---
+++ kernel.cpp
@@ -0,0 +1,16 @@
+void printf(char* str){
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    for(int i = 0; str[i] != '\0'; ++i){
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) |  str[i];
+    }
+}
+
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
+    printf("Hello Fucking World!");
+
+    while(1);
+}
